9c5e251899e05daed1789130c589ef68b9b7d7f8
mwen00 <melanie.a.wen@gmail.com>
2022-11-11 10:57:33 -0800

    adding types and notes

--- zendiggi.py
+++ zendiggi.py
@@ -1,7 +1,10 @@
 import os
+
+import pandas as pd
 from dotenv import load_dotenv
 import praw
 from pprint import pprint
+from google_reddit_scrape import *
 
 
 # Create Reddit instance
@@ -20,8 +23,9 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
 #     print(submission.title)
 
 # Example: Determine Available Attributes of a PRAW Object
-# submission = reddit.submission("39zje0")
-# print(submission.title)  # to make it non-lazy
+submission = reddit.submission("39zje0")
+# print(submission)
+print(submission.title)  # to make it non-lazy
 # pprint(vars(submission))
 
 # Working with Streams
@@ -31,10 +35,28 @@ reddit = praw.Reddit(client_id=CLIENT_ID,
 # for submission in reddit.subreddit("all").stream.submissions():
 #     print(submission)
 
-url = "https://www.reddit.com/r/LosAngeles/comments/be5rj/visiting_la_for_8_days_i_have_tons_of_questions/"
-submission = reddit.submission(url=url)
+# List of test comments
+submissions = ["be5rj", "vmch7l", "r9bqwt", "suu23h", "pojpp5", "cr0z9", "wrr1uc", "sq4p9j", "suebs2", "sgni78"]
+
+# Parsing examples:
+# url = "https://www.reddit.com/r/LosAngeles/comments/be5rj/visiting_la_for_8_days_i_have_tons_of_questions/"
+# submission = reddit.submission(url=url)
 # Or can use: submission = reddit.submission("be5rj")
 
-for top_level_comment in submission.comments:
-    print(top_level_comment.body)
+# TODO: create matrix of Top Level comment and concatenated sub comments
+# top_comments = []
+#
+# for i in range(len(submissions)):
+#     s = reddit.submission(submissions[i])
+#     print(s)
+    # for comment in reddit.submission(submissions[0]).comments:
+    #     top_comments.append(comment)
 
+# df = pd.DataFrame(top_comments, columns=['Top'])
+#
+# print(df)
+
+# print(reddit.submission("be5rj"))
+
+def insert_comments(df):
+    pass
