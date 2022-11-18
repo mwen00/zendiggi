3e03c28f1084d3264e33b12df2739e0486071586
mwen00 <melanie.a.wen@gmail.com>
2022-11-18 02:13:18 -0800

    test visualization

--- ner_parsing.py
+++ ner_parsing.py
@@ -6,18 +6,29 @@ from google_reddit_scrape import *
 nlp = spacy.load("en_core_web_sm")
 
 # Testing with a specific post_id
-TEST_POST_ID = "be5rj"
+# TEST_POST_ID = "be5rj"
 
-# Create a reference to the comments collection
-comments_ref = db.collection(REDDIT_POSTS_COLLECTION).document(TEST_POST_ID).collection(COMMENTS_COLLECTION)
+# Create a reference to collections
+posts_ref = db.collection(REDDIT_POSTS_COLLECTION)
+# comments_ref = db.collection(REDDIT_POSTS_COLLECTION).document(TEST_POST_ID).collection(COMMENTS_COLLECTION)
 
-# Create a query against the collection for the top comment
-query_ref = comments_ref.where("comment_rank", "==", 0).get()
+# Create a query against the desired collection
+posts_query_ref = posts_ref.where("google_search_rank", "==", 0).get()
+# comments_query_ref = comments_ref.where("comment_rank", "==", 0).get()
 
 text = ""
 
-for comment in query_ref:
-    text += comment.to_dict()["top_level_comment"]
+for post in posts_query_ref:
+    print(post.id)
+    comments_ref = db.collection(REDDIT_POSTS_COLLECTION).document(post.id).collection(COMMENTS_COLLECTION)
+    comments_query_ref = comments_ref.where("comment_rank", "==", 0).get()
+
+    for comment in comments_query_ref:
+        data = comment.to_dict()
+        text += data["top_level_comment"] + "\n-----------" + data["replies"]
+
+    text += "\n----------- NEW POST -----------\n"
+
 
 doc = nlp(text)
 displacy.serve(doc, style="ent")
